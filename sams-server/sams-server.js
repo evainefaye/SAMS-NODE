e8ec1a44dd0f8b3a04312ecbcd2ed821175d6778
Stephaie Martinez <stephanie@hawkbane.net>
2017-06-29 20:04:54 -0500

    Removed extraneous console.logs

--- sams-server/sams-server.js
+++ sams-server/sams-server.js
@@ -38,7 +38,6 @@ io.sockets.on('connection', function (socket) {
     // Request the connected client to announce its connection.
     // On the client side this function will share names but have different
     // functions based on it being a SASHA client or a Monitor client
-    console.log('About to emit "Request Connection Type" with ' + socket.connectionId + ' ' + ServerStartTime);
     socket.emit('Request Connection Type', {
         ConnectionId: socket.connectionId,
         ServerStartTime: ServerStartTime
@@ -48,10 +47,8 @@ io.sockets.on('connection', function (socket) {
     // Perform when any user disconnects
     socket.on('disconnect', function() {
         var ConnectionId = socket.connectionId;
-        console.log('disconnect detected');
         if (typeof SashaUsers[ConnectionId] != 'undefined') {
 	    var UserInfo = SashaUsers[ConnectionId];
-	    console.log('disconnect from SASHA user');
             // Remove the SASHA connection from the list of connected users
             delete SashaUsers[ConnectionId];
             // Update the list of connected users on monitor  clients
@@ -60,16 +57,12 @@ io.sockets.on('connection', function (socket) {
                 UserInfo: UserInfo
             });
         }
-        console.log('disconnect not a SASHA user');
     });
-
-
 
     // Store SASHA User Information in SashaUsers Object
     // Add User to sasha room
     // Add User to list of SASHA users on monitor clients
     socket.on('Register SASHA User', function(data) {
-        console.log('Register SASHA User was called');
         // Place in SASHA room 
         socket.join('sasha');
         var ConnectionId = data.ConnectionId;
@@ -77,8 +70,6 @@ io.sockets.on('connection', function (socket) {
         var UTCTime = new Date().toISOString();
         UserInfo.ConnectTime = UTCTime;
         SashaUsers[ConnectionId] = UserInfo;
-        console.log('About to emit Add SASHA Connection to Monitor');
-        console.log (UserInfo);
         io.sockets.in('monitor').emit('Add SASHA Connection to Monitor', {
             ConnectionId: ConnectionId,
             UserInfo: UserInfo
@@ -86,12 +77,10 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('Register Monitor User', function() {
-        console.log('Registered Monitor Type User');
         socket.join('monitor');
     });
 
     socket.on('Notify Server Begin SASHA Flow', function(data) {
-        console.log('In Notify Server Begin SASHA Flow');
         var ConnectionId = socket.connectionId;
         var UserInfo = SashaUsers[ConnectionId];
         if (UserInfo.UserStatus != 'Inactive') {
@@ -113,7 +102,6 @@ io.sockets.on('connection', function (socket) {
         UserInfo.StepHistory.push(StepName);
         UserInfo.StepTime.push(Date.now());
         SashaUsers[ConnectionId] = UserInfo
-        console.log('about to emit Notify Monitor Begin SASHA Flow to monitors');
         io.sockets.in('monitor').emit('Notify Monitor Begin SASHA Flow', {
         	ConnectionId: ConnectionId,
         	UserInfo: UserInfo
@@ -126,33 +114,19 @@ io.sockets.on('connection', function (socket) {
         var StepName = data.StepName;
         var UserInfo = SashaUsers[ConnectionId];
         var UserStatus = UserInfo.UserStatus;
-        console.log('i am good');
         if (UserStatus != 'In Process') {
-            console.log('not in proces');
             return;
         }
-        console.log('was in process');
         UserInfo.FlowName = FlowName;
         UserInfo.StepName = StepName;
-        console.log('stepName of ' + StepName + 'Flowname of ' + FlowName);
         UserInfo.StepStartTime =  new Date().toUTCString();
         UserInfo.FlowHistory.push(FlowName);
         UserInfo.StepHistory.push(StepName);
         UserInfo.StepTime.push(Date.now());
         SashaUsers[ConnectionId] = UserInfo;
-        console.log('about to do one more broadcast to monitors');
         io.sockets.in('monitor').emit('Update Flow and Step Info', {
             ConnectionId: ConnectionId,
             UserInfo: UserInfo
         });
-    });
-
-    // Display Connected SASHA users for newly connected monitor
-    socket.on('Announce Monitor Connection', function() {
-        console.log(SashaUsers);
-        socket.emit('Retrieve SASHA Connected Users', {
-            ServerStartTime: ServerStartTime,
-            SashaUsers: SashaUsers
-	    });
     });
 });
